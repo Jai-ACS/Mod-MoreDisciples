9d06007f045652863f99c515a39a78a4b411b591
Jai <jyai.san+acs@gmail.com>
2025-12-21 23:42:46 +0800

    Update MoreDisciples.lua

--- Scripts/MoreDisciples.lua
+++ Scripts/MoreDisciples.lua
@@ -67,6 +67,11 @@ function OnConfigWindowInit(self)
 	-- titleBox.height = 50
 	-- titleText.y = titleBox.y + (titleBox.height - titleText.height) / 2
 
+	local optionsGroup = self:GetChild("options")
+	optionsGroup:GetChild("option-default").title = XT("默认")
+	optionsGroup:GetChild("option-desktop").title = XT("和电脑版持平")
+	optionsGroup:GetChild("option-max").title = XT("最大增幅")
+
 	Mod.data = Mod.data or {}
 	local selectedIndex = Mod.data.index or 1
 	if selectedIndex < 1 then
